4f9fe650d8b89f7aba7714cca33b58a34c72b134
overclokk <info@overclokk.net>
2020-02-23 10:37:00 +0100

    Bench

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-vendor\bin\phpcbf --ignore=./tests/_support/* ./src/ ./tests/ && vendor\bin\phpcs --ignore=./tests/_support/* ./src/ ./tests/ && vendor\bin\phpstan analyze --level=4 && codecept run unit
+vendor\bin\phpcbf --ignore=./tests/_support/* ./src/ ./tests/ && vendor\bin\phpcs --ignore=./tests/_support/* ./src/ ./tests/ && vendor\bin\phpstan analyze && vendor\bin\psalm && codecept run unit --coverage-txt && vendor\bin\infection
--- tests/benchmarks/AurynResolverBench.php
+++ tests/benchmarks/AurynResolverBench.php
@@ -3,17 +3,21 @@ declare(strict_types=1);
 
 namespace ItalyStrap\Tests\Benchmark;
 
+use ItalyStrap\Config\ConfigFactory;
 use ItalyStrap\Empress\AurynResolver;
+use ItalyStrap\Empress\Injector;
+use stdClass;
 
 class AurynResolverBench {
 
 	/**
+	 * @Warmup(2)
 	 * @Revs(1000)
 	 * @Iterations(5)
 	 */
 	public function benchResolver() {
-		$injector = new \ItalyStrap\Empress\Injector();
-		$config = \ItalyStrap\Config\ConfigFactory::make([
+		$injector = new Injector();
+		$config = ConfigFactory::make([
 			AurynResolver::SHARING	=> [
 				stdClass::class,
 			],
@@ -21,5 +25,18 @@ class AurynResolverBench {
 
 		$resolver = new AurynResolver( $injector, $config );
 		$resolver->resolve();
+
+		$class = $injector->make(stdClass::class);
+	}
+
+	/**
+	 * @Warmup(2)
+	 * @Revs(1000)
+	 * @Iterations(5)
+	 */
+	public function benchResolverP() {
+		$injector = new Injector();
+		$injector->share(stdClass::class);
+		$class = $injector->make(stdClass::class);
 	}
 }
