fabac9010cec8b67d3addac832ea159573ca371b
Enea <info@overclokk.net>
2023-08-16 16:03:24 +0200

    Start cleanup files

--- create_empty_dump.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-wp plugin deactivate --all && wp site empty --yes && wp plugin activate empress && wp db export tests/_data/dump.sql
--- test.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-vendor\bin\phpcbf --ignore=./tests/_support/* ./src/ ./tests/ && vendor\bin\phpcs --ignore=./tests/_support/* ./src/ ./tests/ && vendor\bin\phpstan analyze && vendor\bin\psalm && codecept run unit --coverage-txt && vendor\bin\infection
